d0ea79eb087a56906e6c17c7057a9ecf5b8a1b46
trieuthang1991 <trieuthangtin18a1@gmail.com>
2026-08-14 21:27:59 +0700

    chore: file tam kiem git hook codegraph (se xoa ngay)

---
+++ Services/HookSmokeTestZZZ.cs
@@ -0,0 +1,7 @@
+namespace TourkitAiProxy.Services;
+
+/// File tạm để kiểm git hook có chạy codegraph sync không. Sẽ xoá ngay sau khi kiểm.
+internal static class HookSmokeTestZZZ
+{
+    public static int Answer() => 42;
+}
